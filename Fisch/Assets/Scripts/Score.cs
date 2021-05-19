679615ac3eabfe35922a6ad0143e8f540e92c404
lam12602 <79139004+lam12602@users.noreply.github.com>
2021-05-19 09:43:46 +0100

    edited score script to transition scene

--- Fisch/Assets/Scripts/Score.cs
+++ Fisch/Assets/Scripts/Score.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Score : MonoBehaviour
 {
     public Text scoreDisplay;
     public bool shouldReset = false;
     private static int scoreValue = 0;
+    public int transitionScore;
+    public string nextScene;
 
     private void Start()
     {
@@ -25,5 +28,10 @@ public class Score : MonoBehaviour
 
         scoreDisplay.text = scoreValue.ToString();
 
+        //check if socre is bigger than the score needed to transition between scenes
+        if (scoreValue >= transitionScore)
+        {
+            SceneManager.LoadScene(nextScene);
+        }
     }
 }
