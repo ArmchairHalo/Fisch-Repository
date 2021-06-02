e1131c7189f24b3da67c423cbc22483af726ca4f
ArmchairHalo <79139275+ArmchairHalo@users.noreply.github.com>
2021-06-02 09:15:39 +0100

    Added created assets

---
+++ Fisch/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float movementForce = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        if (Input.GetKey(KeyCode.D) == true)
+        {
+            Rigidbody2D ourRigidbody = GetComponent<Rigidbody2D>();
+
+            ourRigidbody.AddForce(Vector2.right * movementForce);
+        }
+
+        if (Input.GetKey(KeyCode.A) == true)
+        {
+            Rigidbody2D ourRigidbody = GetComponent<Rigidbody2D>();
+
+            ourRigidbody.AddForce(Vector2.left * movementForce);
+        }
+
+        
+}
